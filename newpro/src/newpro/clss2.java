422f5e4f1c7f73823dc845c72fefa6cda66b8f31
Bekigit <berekim@gmail.comgit config --global user.email berekim@gmail.comgit config --global user.email berekim@gmail.com>
2021-06-24 19:50:45 -0400

    file updated.

--- newpro/src/newpro/clss2.java
+++ newpro/src/newpro/clss2.java
@@ -1,5 +1,8 @@
 package newpro;
 
 public class clss2 {
-
+	public static void main(String[] args) {
+		 // TODO Auto-generated method stub
+		 System.out.println("hello world, this is the learning process of java and git.");
+	 }
 }
